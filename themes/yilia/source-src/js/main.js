7a8178575fc695a1e4ba1807ec92a4858cd2f53c
JiHy <jhyqingy@gmail.com>
2016-12-20 12:45:59 +0800

    主题更新，加了几篇文章

--- themes/yilia/source-src/js/main.js
+++ themes/yilia/source-src/js/main.js
@@ -1,61 +1,30 @@
-require('../css/main.scss')
+// 样式
+import '../css/main.scss'
+// 上报
+import './report'
+// fix hexo 不支持的配置
+import Fix from './fix'
+// 图片查看器
+import Viewer from './viewer'
+// 分享
+import Share from './share'
+// 浏览器判断
+import Browser from './browser'
+// 手机
+import Mobile from './mobile'
+// 异步脚本
+import { loadScript } from './util'
 
-require('badjs-report')
-
-// 统计用，开发者不需要理会
-if (window.BJ_REPORT) {
-	BJ_REPORT.init({
-  		id: 1
-	});
-	BJ_REPORT.init({
-		id: 1,                                
-		uin: window.location.origin,          
-		combo: 0,                             
-		delay: 1000,                          
-		url: "//litten.me:9005/badjs/",       
-		ignore: [/Script error/i],           
-		random: 1,                            
-		repeat: 500000,                         
-		onReport: function(id, errObj){},    
-		ext: {}                             
-	});
-	// iframe不上报
-	var host = window.location.host
-	var isNotFrame = (top === window)
-	var isNotLocal = !((/localhost/i.test(host) || /127.0.0.1/i.test(host) || /0.0.0.0/i.test(host)))
-	isNotFrame && isNotLocal && BJ_REPORT.report('yilia-' + window.location.host)
+window.onload = () => {
+	if(Browser.versions.mobile && window.screen.width < 800){
+		Mobile.init()
+	}else{
+		setTimeout(function() {
+			loadScript('/slider.js?v=4.0.0.js')
+		}, 80)
+	}
+	Fix.init()
+	Share.init()
+	Viewer.init()
 }
 
-require('./jquery')
-var tags = require('./tags')
-var archiveInner = require('./archive-inner')
-var tools = require('./tools')
-var browser = require('./browser')
-var fixPage = require('./fix-page')
-var mobile = require('./mobile')
-var viewer = require('./viewer')
-require('./jquery.lazyload')
-
-$(function() {
-	viewer.init()
-	archiveInner.init()
-	fixPage.init()
-	tags.init()
-	// todo: resize destrop
-	if(browser.versions.mobile === true && $(window).width() < 800){
-		mobile.init()
-	}else{
-		tools.init()
-		$('.js-smart-menu').click(function(e) {
-			e.stopPropagation()
-			tools.show($(this).data('idx'))
-		})
-		$('.left-col,.mid-col').click(function() {
-			tools.hide()
-		})
-	}
-
-	if (yiliaConfig && yiliaConfig.open_in_new) {
-		$('.article-entry a').not('.article-more-a').attr('target', '_blank')
-	}
-})
